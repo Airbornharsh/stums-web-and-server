f77acbb4bf6e38c61f9ed7202172f7eaf3d39da1
Harsh Keshri <harshkeshri1234567@gmail.com>
2023-03-16 01:53:43 +0530

    Updated

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ module.exports = {
   async headers() {
     return [
       {
-        source: '/*',
+        source: '/:path*',
         headers: [
           {
             key: 'Access-Control-Allow-Origin',
