81b8a49c9d65c3651f97f884dbf609cb7789d008
Harsh Keshri <harshkeshri1234567@gmail.com>
2023-03-16 01:47:37 +0530

    Configured Next Config File

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
+module.exports = {
+  async headers() {
+    return [
+      {
+        source: '/about',
+        headers: [
+          {
+            key: 'x-custom-header',
+            value: 'my custom header value',
+          },
+          {
+            key: 'x-another-custom-header',
+            value: 'my other custom header value',
+          },
+        ],
+      },
+    ]
+  },
 }
-
-module.exports = nextConfig
